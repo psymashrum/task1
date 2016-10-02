599e34b90af0ad24fe0f71f829c119f199aced69
Alex Elkin <alex.elkin.name@gmail.com>
2016-10-02 14:34:57 +0600

    last stage

--- go.sh
+++ go.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-GIT = "git@github.com:psymashrum/task1.git"
+GIT=git@github.com:psymashrum/task1.git
 
 mkdir /tmp
 mkdir /tmp/code
-cd /tmp/code
 
+cd /tmp/code
 git clone $GIT
+
 echo "Success download"
+
 docker-compose build
 docker build -t ubuntu /tmp/code/
 docker run -d --name ubuntu -p 80:80 ubuntu:latest
