f4a2468ddc874cf247d2f3b94f31ce7be7c08b00
Alex Elkin <alex.elkin.name@gmail.com>
2016-10-06 12:27:01 +0600

    Add files via upload

--- manifests/tomcat.pp
+++ manifests/tomcat.pp
@@ -11,9 +11,6 @@ tomcat::config::server::connector { 'tomcat8':
   catalina_base         => '/opt/tomcat8',
   port                  => '8080',
   protocol              => 'HTTP/1.1',
-  additional_attributes => {
-    'redirectPort' => '8443'
-  },
 }
 tomcat::config::server { 'tomcat8':
   address		=> '127.0.0.1',
